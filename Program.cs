3afd88827f0746acd7f68879387b61587fa138e3
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-06-24 17:05:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Slotkin_ConstructorChainingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student molly = new Student("Molly Cat", 1234);
+
+            Console.WriteLine("molly's Id Number: {0}", molly.GetIdNumber());
+            Console.WriteLine("molly's Name: {0}", molly.GetName());
+
+            Student bella = new Student("Bella");
+
+            Console.WriteLine("bella’s Id Number: {0}", bella.GetIdNumber());
+            Console.WriteLine("bella’s Name: {0}", bella.GetName());
+
+            Student kitKat = new Student("KitKat");
+
+            Console.WriteLine("kitKat’s Id Number: {0}", kitKat.GetIdNumber());
+            Console.WriteLine("kitKat’s Name: {0}", kitKat.GetName());
+        }
+    }
+}
